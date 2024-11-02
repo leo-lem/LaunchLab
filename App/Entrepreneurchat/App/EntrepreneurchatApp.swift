8a58efc06b31adf6edf02dfa47329159a5ac2d9a
3lemmerm <leopold.lemmermann@studium.uni-hamburg.de>
2024-11-02 14:04:41 +0100

    Add base iOS app.
    * should have working styleguide and uicomponents
    
    * add styleguide
    
    * added first view from uicomponents
    
    * added submodule for uicomponents and styleguide
    
    * added files
    
    * adjusted signing to use provisioning profiles
    
    * update run script, assets and entitlements path to new path
    
    * refactored submodules styleguide and uicomponents
    
    * - Gitignored and removed xcuserdata.
    - Removed Maxi's personal account from project configuration.
    
    * added swiftlint, swiftformat, IDETemplateMacros for Copyright disclaimer on file creation
    
    * added run script for swiftgen to execute on build
    
    * added uicomponents and styleguide package, added swiftgen config
    
    * rebase into /App folder
    
    * initialized project
    
    * Initial Commit

---
+++ App/Entrepreneurchat/App/EntrepreneurchatApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © 2024 M-Lab Group Entrepreneurchat, University of Hamburg, Transferagentur. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct EntrepreneurchatApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ App/Entrepreneurchat/Styleguide/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "Styleguide",
+  defaultLocalization: "en",
+  platforms: [.iOS(.v17)],
+  products: [
+    .library(
+      name: "Styleguide",
+      targets: ["Styleguide"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "Styleguide"
+    )
+  ]
+)
