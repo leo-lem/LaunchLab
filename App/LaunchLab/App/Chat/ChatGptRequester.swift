9aba29fe58e2aeaa5439c407e5950c5b4483b4b7
Henrik Marcussen <henrik.marcussen@studium.uni-hamburg.de>
2025-01-19 21:49:25 +0100

    added generateDocumentString for Document generation

--- App/LaunchLab/App/Chat/ChatGptRequester.swift
+++ App/LaunchLab/App/Chat/ChatGptRequester.swift
@@ -11,6 +11,11 @@ import Styleguide
 class ChatGPTRequester {
   private let key = L10n.apiKey
   private let url = URL(string: "https://api.openai.com/v1/chat/completions")!
+  private let contentPromptRel = """
+        You are a digital co-founder. Always act professionally.
+        Your job is to help the user to progress. I give you module titles and always questions and answers
+        for the modules that the user has answered. A module has several questions with answers. Consider them when answering the questions.
+  """
 
   /// fetiching `modules`
   private func fetchModules() -> [Module] {
@@ -45,14 +50,34 @@ class ChatGPTRequester {
   }
 
   func getHelpFromCoFounder(moduleTitle: String, moduleContentTitle: String) async -> String? {
-    let contentPromptRaw = """
-    You are a digital co-founder. Always act professionally.
-    Your job is to help the user to progress. I give you module titles and always questions and answers
-    for the modules that the user has answered. A module has several questions with answers. Consider them when answering the questions.
-    Answer in bullet points.
-    """
+    let contentPromptRaw = contentPromptRel + "\nAnswer in bullet points."
     return await sendMessage(
       userPrompt: "The name of the module is: \(moduleTitle). The question is: \(moduleContentTitle)",
+      contentPrompt: generateContentPrompt(
+        from: fetchModules(),
+        contentPromptRaw: contentPromptRaw
+      )
+    )
+  }
+
+  private func getUserPromptForDocumentType(documentType: String) -> String {
+      """
+      Create a document of type "\(documentType)". Take the information you got from the systemPrompt.
+
+      Formatting guidelines:
+      - Elevator Pitch: Short introduction, problem definition, solution, call to action.
+      - Pitch Deck: Overview of problem, solution, target market, business model, team and call to action.
+      - Business Plan: Detailed description of problem, solution, target market, business model, marketing strategy, financial plan and risk analysis.
+
+      Return the result in clear paragraphs that can be easily inserted into a PDF.
+      """
+  }
+
+  func generateDocumentString(documentType: String) async -> String? {
+    let contentPromptRaw: String = contentPromptRel + "\nAnswer in clear paragraphs for a PDF."
+
+    return await sendMessage(
+      userPrompt: getUserPromptForDocumentType(documentType: documentType),
       contentPrompt: generateContentPrompt(
         from: fetchModules(),
         contentPromptRaw: contentPromptRaw
